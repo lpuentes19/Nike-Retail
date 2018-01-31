a3f468a81372a2100f8bbecee78fc2722ae54a41
lpuentes19 <luey.puentes@gmail.com>
2018-01-30 17:46:31 -0700

    Added the submitButtonCell in the cellForRowAt method and adjusted the numberOfRowsInSection from 4 to 5. Also, changed the names of the identifiers of both billingLabelCell and billingInfoCell
    :q

--- Nike-Retail/Controllers/Checkout/CheckoutTableViewController.swift
+++ Nike-Retail/Controllers/Checkout/CheckoutTableViewController.swift
@@ -12,8 +12,8 @@ class CheckoutTableViewController: UITableViewController {
 
     // ReuseIdentifiers
     struct ReuseIdentifiers {
+        static let billingLabelCell = "billingLabelCell"
         static let billingInfoCell = "billingInfoCell"
-        static let creditCardInfoCell = "creditCardInfoCell"
         static let totalDetailCell = "totalDetailCell"
         static let totalCell = "totalCell"
         static let submitButtonCell = "submitButtonCell"
@@ -26,17 +26,17 @@ class CheckoutTableViewController: UITableViewController {
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return 5
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: ReuseIdentifiers.billingInfoCell, for: indexPath)
+            let cell = tableView.dequeueReusableCell(withIdentifier: ReuseIdentifiers.billingLabelCell, for: indexPath)
             
             return cell
             
         } else if indexPath.row == 1 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: ReuseIdentifiers.creditCardInfoCell, for: indexPath)
+            let cell = tableView.dequeueReusableCell(withIdentifier: ReuseIdentifiers.billingInfoCell, for: indexPath)
             
             return cell
             
@@ -50,10 +50,13 @@ class CheckoutTableViewController: UITableViewController {
             
             return cell
             
-        } else {
+        } else if indexPath.row == 4 {
             let cell = tableView.dequeueReusableCell(withIdentifier: ReuseIdentifiers.submitButtonCell, for: indexPath)
             
             return cell
+            
+        } else {
+            return UITableViewCell()
         }
     }
 }
