8eff08dddc927618255e541d4364264461bef8cb
lpuentes19 <luey.puentes@gmail.com>
2018-01-30 17:15:57 -0700

    Created a CreditCardiInfoTableViewCell file and subclassed the credit card info cell to this.

---
+++ Nike-Retail/Views/Checkout/CreditCardInfoTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CreditCardInfoTableViewCell.swift
+//  Nike-Retail
+//
+//  Created by Luis Puentes on 1/30/18.
+//  Copyright © 2018 LuisPuentes. All rights reserved.
+//
+
+import UIKit
+
+class CreditCardInfoTableViewCell: UITableViewCell {
+
+
+}
