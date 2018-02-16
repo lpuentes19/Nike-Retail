1304e4e52593636481389b73c7eadec8573e2fd3
lpuentes19 <luey.puentes@gmail.com>
2018-02-15 21:03:05 -0700

    Completed the LoginTBVC

--- Nike-Retail/Models/LoginTableViewController.swift
+++ Nike-Retail/Models/LoginTableViewController.swift
@@ -7,10 +7,59 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginTableViewController: UITableViewController {
-
+    
+    // MARK: - Storyboard Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        emailTextField.becomeFirstResponder()
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+    
+    // MARK: - Functions
+    func alert(title: String, message: String, buttonTitle: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: buttonTitle, style: .default, handler: nil)
+        alertVC.addAction(action)
+        present(alertVC, animated: true, completion: nil)
+    }
+    
+    // MARK: - Storyboard Actions
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        if emailTextField.text != "" && (passwordTextField.text?.characters.count)! > 6 {
+            let email = emailTextField.text!
+            let password = passwordTextField.text!
+            
+            Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+                if let error = error {
+                    self.alert(title: "Oops!", message: error.localizedDescription, buttonTitle: "OK")
+                } else {
+                    self.dismiss(animated: true, completion: nil)
+                }
+            })
+        }
+    }
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension LoginTableViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailTextField {
+            passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            passwordTextField.resignFirstResponder()
+            
+        }
+        return true
     }
 }
